dada4876ce5ad2b05d1dabea7086935abf225625
u5u5u5u <kyutech.joko@gmail.com>
2023-11-01 23:08:21 +0900

    Prettier_適応

--- kcl-hack/src/pages/index.tsx
+++ kcl-hack/src/pages/index.tsx
@@ -1,16 +1,13 @@
-'use client'
+"use client";
 import React, { useState } from "react";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
-    return (
-      
-      <main>
-       <Link href="/search">
-        <button>
-          scanへ移動する
-        </button>
-       </Link>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <Link href="/search">
+        <button>scanへ移動する</button>
+      </Link>
+    </main>
+  );
+}
