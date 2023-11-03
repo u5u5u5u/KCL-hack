01754c15f774d61fe41b4c92ee657fbf17e20954
u5u5u5u <kyutech.joko@gmail.com>
2023-11-04 02:42:41 +0900

    install_material-ui

--- kcl-hack/src/pages/scan/scan-select.tsx
+++ kcl-hack/src/pages/scan/scan-select.tsx
@@ -1,27 +1,34 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 export default function Home() {
   return (
     <main>
-      <h2 className="text-center">SCAN</h2>
-      <div className="flex justify-center items-stretch">
-        <div className="px-4 py-4">
-          <Link href="/scan/search">
-            <button>読み取り</button>
-          </Link>
+      <div className="h-screen flex justify-center items-center">
+        <div className="flex flex-col">
+          <h2 className="text-7xl text-center font-mono my-10">SCAN</h2>
+          <div className="flex justify-center items-stretch">
+            <div className="text-5xl border px-5 mx-5 py-5 my-5">
+              <Link href="/scan/search">
+                <button>読み取り</button>
+              </Link>
+            </div>
+            <div className="text-5xl mx-20 my-5">
+              <Link href="/scan/search">
+                <button>入力</button>
+              </Link>
+            </div>
+          </div>
+          <div className="text-3xl text-center my-10">
+            <Link href="/home">
+              <button>戻る</button>
+            </Link>
+          </div>
         </div>
-        <div className="px-4 py-4">
-          <Link href="/scan/search">
-            <button>入力</button>
-          </Link>
-        </div>
-      </div>
-      <div className="text-center">
-        <Link href="/home">
-          <button>戻る</button>
-        </Link>
       </div>
     </main>
   );
