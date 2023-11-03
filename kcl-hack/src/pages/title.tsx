130960264073cf8907e0a09ac6aeac624ad1ced9
51hamu51 <sakateni20200504@icloud.com>
2023-11-03 12:54:19 +0900

    プロトタイプ完成したを

--- kcl-hack/src/pages/title.tsx
+++ kcl-hack/src/pages/title.tsx
@@ -5,9 +5,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="center color-red">BAGOLA;</h1>
-      <Link href="/search" className="center">
-        <button>ログイン</button>
+      <h1 className="text-9xl text-red-600 text-center italic">BAGOLA;</h1>
+      <Link href="/hoge">
+        <div className="text-center">
+          <button className="text-3xl text-blue-500">ログイン</button>
+        </div>
       </Link>
     </main>
   );
