7cd3d7f42c0dcdac76437330e26d7be99886ec21
51hamu51 <sakateni20200504@icloud.com>
2023-11-03 13:16:02 +0900

    href_change

--- kcl-hack/src/pages/title.tsx
+++ kcl-hack/src/pages/title.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-9xl text-red-600 text-center italic">BAGOLA;</h1>
-      <Link href="/hoge">
+      <Link href="/login">
         <div className="text-center">
           <button className="text-3xl text-blue-500">ログイン</button>
         </div>
