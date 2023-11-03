0564834202092499f331330201b4132afdcf4553
semi-koron <6jimoto6@gmail.com>
2023-11-04 00:14:05 +0900

    コピー

--- kcl-hack/src/pages/index.tsx
+++ kcl-hack/src/pages/index.tsx
@@ -5,12 +5,25 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <Link href="/search">
-        <button>scanへ移動する</button>
-      </Link>
-      <Link href="/login">
-        <button>loginへ移動する</button>
-      </Link>
+      <div className="h-screen flex justify-center items-center">
+        <div className="flex flex-col">
+          <h1 className="text-9xl text-red-600 text-center font-serif italic">
+            BAGOLA;
+          </h1>
+          <div className="text-center mt-20">
+            <Link href="/login">
+              <button className="text-3xl text-blue-500 font-mono">
+                ログイン
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="text-center">
+        <Link href="/home">
+          <button>ホームへ</button>
+        </Link>
+      </div>
     </main>
   );
 }
--- kcl-hack/src/pages/search.tsx
+++ kcl-hack/src/pages/search.tsx
@@ -3,27 +3,78 @@ import React, { useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
+  const [number, setNum] = useState<string>("");
+  const [name, setNam] = useState<string>("");
+  const [price, setPri] = useState<number>();
+  const [image, setIma] = useState<string>("");
+  const yahookey = "dj00aiZpPVpKZG9FdVNJbW5SRCZzPWNvbnN1bWVyc2VjcmV0Jng9ZWE-";
+
+  const changeNum = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+    setNum(event.target.value);
+  };
+
+  async function fetchname() {
+    try {
+      const res = await fetch(
+        `https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid=${yahookey}&jan_code=${number}`
+      );
+      if (!res.ok) {
+        throw new Error("fetchに失敗しました");
+      }
+      const data = await res.json();
+      console.log(data);
+      setNam(data.hits[0].name);
+      setPri(data.hits[0].price);
+      setIma(data.hits[0].image.large);
+    } catch (error) {
+      console.error("エラーです:", error);
+    }
+  }
+
+  const sendNum = () => {
+    console.log(number);
+    fetchname();
+    setNam("");
+    setNum("");
+    setIma("");
+    setPri(NaN);
+  };
+
   return (
     <main>
       <div className="h-screen flex justify-center items-center">
         <div className="flex flex-col">
-          <h1 className="text-9xl text-red-600 text-center font-serif italic">
-            BAGOLA;
-          </h1>
-          <div className="text-center mt-20">
-            <Link href="/login">
-              <button className="text-3xl text-blue-500 font-mono">
-                ログイン
-              </button>
+          <h2 className="text-6xl text-center mb-10">
+            <span className="font-mono">JANコード</span>を入力
+          </h2>
+          <div className="flex justify-center mb-6">
+            <input
+              className="text-6xl text-red-500 mr-10"
+              type="text"
+              value={number}
+              onChange={changeNum}
+            />
+            <button className="text-5xl" onClick={sendNum}>
+              送信
+            </button>
+          </div>
+          <div>
+            <img className="" src={image} />
+          </div>
+          <p className="text-4xl my-5">商品名 : {name}</p>
+          <p className="text-4xl">価格 : {price} 円</p>
+          <div className="text-6xl text-center my-10">
+            <button>登録</button>
+          </div>
+          <div className="text-3xl text-center my-5">
+            <Link href="/home">
+              <button>戻る</button>
             </Link>
           </div>
         </div>
       </div>
-      <div className="text-center">
-        <Link href="/home">
-          <button>ホームへ</button>
-        </Link>
-      </div>
     </main>
   );
 }
+4909411073114;
