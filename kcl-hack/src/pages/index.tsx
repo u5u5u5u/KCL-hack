e112fc4e988921112639db5508b1d9f317197e2c
51hamu51 <sakateni20200504@icloud.com>
2023-11-03 13:11:53 +0900

    button

--- kcl-hack/src/pages/index.tsx
+++ kcl-hack/src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <Link href="/login">
+      <Link href="/search">
         <button>scanへ移動する</button>
       </Link>
     </main>
