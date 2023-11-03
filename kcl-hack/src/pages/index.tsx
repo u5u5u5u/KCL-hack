b39532699071a94a4fee76817786b25d3ef24bd3
u5u5u5u <kyutech.joko@gmail.com>
2023-11-03 13:33:41 +0900

    change: scan_page

--- kcl-hack/src/pages/index.tsx
+++ kcl-hack/src/pages/index.tsx
@@ -5,9 +5,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <Link href="/search">
-        <button>scanへ移動する</button>
-      </Link>
+      <h2 className="text-center">SCAN</h2>
+      <div className="flex justify-center items-stretch">
+        <div className="px-4 py-4">
+          <Link href="/search">
+            <button>読み取り</button>
+          </Link>
+        </div>
+        <div className="px-4 py-4">
+          <Link href="/search">
+            <button>入力</button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
