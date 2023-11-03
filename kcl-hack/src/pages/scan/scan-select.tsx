98305b2187b0f596a5119e9dabe56dea4fb2687d
u5u5u5u <kyutech.joko@gmail.com>
2023-11-04 04:32:07 +0900

    change: button

--- kcl-hack/src/pages/scan/scan-select.tsx
+++ kcl-hack/src/pages/scan/scan-select.tsx
@@ -9,14 +9,14 @@ export default function Home() {
     <main>
       <div className="h-screen flex justify-center items-center">
         <div className="flex flex-col">
-          <h2 className="text-7xl text-center font-mono my-10">SCAN</h2>
+          <h2 className="text-8xl text-center font-mono my-10">SCAN</h2>
           <div className="flex justify-center items-stretch">
-            <div className="text-5xl border px-5 mx-5 py-5 my-5">
+            <div className="text-5xl border rounded-2xl px-3 mx-5 py-5 my-5">
               <Link href="/camera">
                 <button>読み取り</button>
               </Link>
             </div>
-            <div className="text-5xl mx-20 my-5">
+            <div className="text-5xl border rounded-2xl px-5 mx-10 py-5 my-5">
               <Link href="/scan/search">
                 <button>入力</button>
               </Link>
