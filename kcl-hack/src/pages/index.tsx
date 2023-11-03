5ec246fadc45443616fea8a54a60f0260c85aad4
u5u5u5u <kyutech.joko@gmail.com>
2023-11-03 16:41:58 +0900

    change: index_style

--- kcl-hack/src/pages/index.tsx
+++ kcl-hack/src/pages/index.tsx
@@ -5,11 +5,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-9xl text-red-600 text-center italic">BAGOLA;</h1>
-      <div className="text-center">
-        <Link href="/login">
-          <button className="text-3xl text-blue-500">ログイン</button>
-        </Link>
+      <div className="h-screen flex justify-center items-center">
+        <div className="flex flex-col">
+          <h1 className="text-9xl text-red-600 text-center font-serif italic">
+            BAGOLA;
+          </h1>
+          <div className="text-center mt-20">
+            <Link href="/login">
+              <button className="text-3xl text-blue-500 font-mono">
+                ログイン
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
       <div className="text-center">
         <Link href="/home">
