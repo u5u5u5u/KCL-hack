4ec458eff38c3a2f607d5b52c02ff729c721c23a
u5u5u5u <kyutech.joko@gmail.com>
2023-11-07 10:01:35 +0900

    change: home_tag

--- kcl-hack/src/pages/home.tsx
+++ kcl-hack/src/pages/home.tsx
@@ -6,28 +6,28 @@ export default function Home() {
   return (
     <main>
       <div className="container">
-        <div className="wrapper">
-          <div className="content">
+        <ul className="wrapper">
+          <li className="content">
             <Link href="/profile">
               <button className="advance-button">プロフィール</button>
             </Link>
-          </div>
-          <div className="content">
+          </li>
+          <li className="content">
             <Link href="/scan/scan-select">
               <button className="advance-button">スキャン</button>
             </Link>
-          </div>
-          <div className="content">
+          </li>
+          <li className="content">
             <Link href="/hoge">
               <button className="advance-button">設定</button>
             </Link>
-          </div>
-          <div className="content">
+          </li>
+          <li className="content">
             <Link href="/battle">
               <button className="advance-button">対戦</button>
             </Link>
-          </div>
-        </div>
+          </li>
+        </ul>
       </div>
     </main>
   );
