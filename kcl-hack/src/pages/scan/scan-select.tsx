b4dc39b0765ed546ac50e163b68aa00d9c896fe0
u5u5u5u <kyutech.joko@gmail.com>
2023-11-07 11:29:22 +0900

    change: scan-select_css

--- kcl-hack/src/pages/scan/scan-select.tsx
+++ kcl-hack/src/pages/scan/scan-select.tsx
@@ -1,27 +1,24 @@
 "use client";
 import Link from "next/link";
 
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-
 export default function Home() {
   return (
     <main>
-      <div className="h-screen flex justify-center items-center">
-        <div className="flex flex-col">
-          <h2 className="text-8xl text-center font-mono my-10">SCAN</h2>
-          <div className="flex justify-center items-stretch">
-            <div className="text-5xl border rounded-2xl px-3 mx-5 py-5 my-5">
+      <div className="container">
+        <div className="wrapper">
+          <h2 className="sub-title">SCAN</h2>
+          <ul className="">
+            <li className="content">
               <Link href="/camera">
-                <button>読み取り</button>
+                <button className="advance-button">読み取り</button>
               </Link>
-            </div>
-            <div className="text-5xl border rounded-2xl px-5 mx-10 py-5 my-5">
+            </li>
+            <li className="content">
               <Link href="/scan/search">
-                <button>入力</button>
+                <button className="advance-button">入力</button>
               </Link>
-            </div>
-          </div>
+            </li>
+          </ul>
           <div className="text-3xl text-center my-10">
             <Link href="/home">
               <button>戻る</button>
