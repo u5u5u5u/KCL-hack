d03ccb515326027f58525bb1f4120bd9756fd1ee
u5u5u5u <kyutech.joko@gmail.com>
2023-11-15 13:10:58 +0900

    change: back-arrow

--- kcl-hack/src/components/footer.tsx
+++ kcl-hack/src/components/footer.tsx
@@ -1,11 +1,11 @@
 import router from "next/router";
-import styles from "./header.module.css";
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.container}>
-      <div>
-        <span className={styles.arrow} onClick={() => router.back()}></span>
+      <div className={styles.back} onClick={() => router.back()}>
+        <span className={styles.arrow} />
       </div>
     </footer>
   );
