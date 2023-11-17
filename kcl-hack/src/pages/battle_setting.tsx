c802f14e7fb561be3a6c7bc580d47fbf7b50a5d7
51hamu51 <sakateni20200504@icloud.com>
2023-11-17 14:15:23 +0900

    なんの成果もgit add .　得られませんでしたgit add .

---
+++ kcl-hack/src/pages/battle_setting.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Link from "next/link";
+import Header from "../components/header";
+import Button from "../components/button";
+import Footer from "../components/footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Header children="ちゃんと設定しようね" />
+      <h2>準備中だよ！！</h2>
+
+      <Footer />
+    </main>
+  );
+}
