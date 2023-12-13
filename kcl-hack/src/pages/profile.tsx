cab658dbe0bc9191bcd2de126bf01d3ef48de874
u5u5u5u <kyutech.joko@gmail.com>
2023-12-14 08:06:05 +0900

    change: text

--- kcl-hack/src/pages/profile.tsx
+++ kcl-hack/src/pages/profile.tsx
@@ -76,7 +76,7 @@ export default function Home() {
       <div className="container">
         <div className="wrapper">
           <div className="wrapper">
-            <label className={styles.label}>ユーザーネーム</label>
+            <label className={styles.label}>User Name</label>
             <input
               className={styles.user_name}
               type="text"
@@ -85,7 +85,7 @@ export default function Home() {
             ></input>
           </div>
           <div className="wrapper">
-            <label className={styles.label}>ひとこと</label>
+            <label className={styles.label}>Comment</label>
             <input
               className={styles.one_word}
               placeholder="よろしくお願いします"
