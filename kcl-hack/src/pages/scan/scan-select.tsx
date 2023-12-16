399877283df5718265d339865e5eb8c1b364b164
u5u5u5u <kyutech.joko@gmail.com>
2023-12-16 15:01:52 +0900

    update: sytle

--- kcl-hack/src/pages/scan/scan-select.tsx
+++ kcl-hack/src/pages/scan/scan-select.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Header children="SCAN" />
       <div className="container">
         <div className="wrapper">
-          <ul className="">
+          <ul className="wrapper">
             <li className="content">
               <Link href="/camera">
                 <Button label="読み取り" />
