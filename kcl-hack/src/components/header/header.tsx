3d28126ef6acd5215731dc795e8578c9306d685b
u5u5u5u <kyutech.joko@gmail.com>
2023-12-19 09:39:53 +0900

    change: header

--- kcl-hack/src/components/header/header.tsx
+++ kcl-hack/src/components/header/header.tsx
@@ -10,19 +10,28 @@ interface HeaderProps {
 const Header = ({ children }: HeaderProps) => {
   return (
     <header className={styles.container}>
-      <div>
+      <div className={styles.logo}>
         <Link href="/">
           <Image
-            className={styles.logo_small}
+            className={styles.logo_pc}
             src="/BAGOLA_logo.png"
+            layout="intrinsic"
             width={247}
+            height={70}
+            alt="BAGOLA"
+          />
+          <Image
+            className={styles.logo_sp}
+            src="/BAGOLA_small.png"
+            layout="intrinsic"
+            width={70}
             height={70}
             alt="BAGOLA"
           />
         </Link>
       </div>
       <div className={styles.title}>{children}</div>
-      <div className={styles.ml_175}>
+      <div className={styles["home-button"]}>
         <Link href="../home">
           <span className={styles.home}></span>
         </Link>
